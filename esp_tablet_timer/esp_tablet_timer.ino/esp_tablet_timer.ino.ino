a18e2bc4fe44f39a291ef84827e504459e935da1
anykeynl <the.anykey@gmail.com>
2018-06-28 22:58:30 +0200

    changed from NTP to http request to get day of week

--- esp_tablet_timer/esp_tablet_timer.ino/esp_tablet_timer.ino.ino
+++ esp_tablet_timer/esp_tablet_timer.ino/esp_tablet_timer.ino.ino
@@ -3,7 +3,7 @@
  * Tablet Timer Project
  * Written by Richard Garsthagen - the.anykey@gmail.com
  * 
- * Version 1.0 - June 27th 2018
+ * Version 1.1 - June 28th 2018
  * https://github.com/AnykeyNL/IpadTimer
  * --------------------------------------------------------------------------------------------------------------------
 
@@ -34,13 +34,6 @@ MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance
 
 int sumids = 0;
 
-unsigned int localPort = 2390;      // local port to listen for UDP packets
-const char* ntpServerName = "europe.pool.ntp.org";
-IPAddress timeServerIP;
-const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
-byte packetBuffer[ NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
-WiFiUDP udp;
-
 long timeNow = 0;
 long timeLast = 0;
 long DefaultTime = 10810; // Default time is 3 hours and 10 seconds.
@@ -64,7 +57,7 @@ int Cards[][5] = {  {0xA6, 0xF8, 0x83, 0x63, 254}, // reset used cards
                 };
 int TotalCards = sizeof(Cards) / (sizeof(int)*5);
 
-int Days[8] = {0,0,0,660,3600,10810,10810,10810}; // noday, monday, tuesday, wednessday, thursday, friday, saturday, sunday
+int Days[8] = {0,0,0,0,0,10810,10810,10810}; // noday, monday, tuesday, wednessday, thursday, friday, saturday, sunday
 
 void setup() {
   Serial.begin(115200);  // Initialize serial communications with the PC
@@ -97,14 +90,15 @@ void setup() {
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());  
 
-  Serial.println("Starting UDP fo time requests (NTP)");
-  udp.begin(localPort);
-  Serial.print("Local port: ");
-  Serial.println(udp.localPort());
-  
-  ShowDay();
+  Serial.println("");
+  Serial.println("Checking day");
+  ShowDay(LastDay);
   CheckDay();
-       
+  ShowDay(LastDay);
+  Serial.print("day is: ");
+  Serial.println(LastDay);
+  delay(3000);
+         
   timeLeft = GetLastTime();
   lastMinuteStored = timeLeft;
       
@@ -132,10 +126,11 @@ void setup() {
  }
 
 void CheckDay(){
-    byte day = GetDay();
-
-    LastDay = EEPROM.read(0);
-    if (LastDay > 0 && LastDay < 8){
+  byte day=0;
+  day = GetDay();
+  
+  LastDay = EEPROM.read(0);
+    if (LastDay >= 0 && LastDay < 8){
        if (LastDay != day) {
         ResetDay(day);
        }
@@ -181,91 +176,33 @@ void ResetDay(byte day){
     SaveLastTime(timeLeft);
 }
 
-unsigned long sendNTPpacket(IPAddress& address)
-{
-  Serial.println("sending NTP packet...");
-  // set all bytes in the buffer to 0
-  memset(packetBuffer, 0, NTP_PACKET_SIZE);
-  // Initialize values needed to form NTP request
-  // (see URL above for details on the packets)
-  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
-  packetBuffer[1] = 0;     // Stratum, or type of clock
-  packetBuffer[2] = 6;     // Polling Interval
-  packetBuffer[3] = 0xEC;  // Peer Clock Precision
-  // 8 bytes of zero for Root Delay & Root Dispersion
-  packetBuffer[12]  = 49;
-  packetBuffer[13]  = 0x4E;
-  packetBuffer[14]  = 49;
-  packetBuffer[15]  = 52;
-
-  // all NTP fields have been given values, now
-  // you can send a packet requesting a timestamp:
-  udp.beginPacket(address, 123); //NTP requests are to port 123
-  udp.write(packetBuffer, NTP_PACKET_SIZE);
-  udp.endPacket();
-}
-
 int GetDay(){
-  WiFi.hostByName(ntpServerName, timeServerIP); 
-  sendNTPpacket(timeServerIP); // send an NTP packet to a time server
-  // wait to see if a reply is available
-  Serial.println("Getting Time");
-  int cb = udp.parsePacket();
-  while (!cb){
-    if (!cb) {
-      Serial.println("no time packet yet");
-      delay(100);
-      cb = udp.parsePacket();
+ int day =0;
+ 
+  HTTPClient http;
+
+  const char* headerNames[] = { "date" };
+  http.collectHeaders(headerNames, sizeof(headerNames)/sizeof(headerNames[0]));  
+  http.begin("http://www.google.nl");
+  int httpCode = http.GET();
+  if  (httpCode > 0){
+     
+    if (http.hasHeader("date"))
+    {
+      String getdate = http.header("date");
+      Serial.println(getdate);
+      if (getdate.startsWith("Mon")) { day = 1;}
+      if (getdate.startsWith("Tue")) { day = 2;}
+      if (getdate.startsWith("Wed")) { day = 3;}
+      if (getdate.startsWith("Thu")) { day = 4;}
+      if (getdate.startsWith("Fri")) { day = 5;}
+      if (getdate.startsWith("Sat")) { day = 6;}
+      if (getdate.startsWith("Sun")) { day = 7;}
     }
   }
-  
-    Serial.print("packet received, length=");
-    Serial.println(cb);
-    // We've received a packet, read the data from it
-    udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
+  http.end();
 
-    //the timestamp starts at byte 40 of the received packet and is four bytes,
-    // or two words, long. First, esxtract the two words:
-
-    unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
-    unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
-    // combine the four bytes (two words) into a long integer
-    // this is NTP time (seconds since Jan 1 1900):
-    unsigned long secsSince1900 = highWord << 16 | lowWord;
-    Serial.print("Seconds since Jan 1 1900 = " );
-    Serial.println(secsSince1900);
-
-    // now convert NTP time into everyday time:
-    Serial.print("Unix time = ");
-    // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
-    const unsigned long seventyYears = 2208988800UL;
-    // subtract seventy years:
-    unsigned long epoch = secsSince1900 - seventyYears;
-    // print Unix time:
-    Serial.println(epoch);
-
-    // Calculate Day of week
-    byte day =  int(((epoch / 86400) + 4) % 7);
-    Serial.print("Day of week: ");
-    Serial.println(day);
-
-    // print the hour, minute and second:
-    Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
-    Serial.print((epoch  % 86400L) / 3600); // print the hour (86400 equals secs per day)
-    Serial.print(':');
-    if ( ((epoch % 3600) / 60) < 10 ) {
-      // In the first 10 minutes of each hour, we'll want a leading '0'
-      Serial.print('0');
-    }
-    Serial.print((epoch  % 3600) / 60); // print the minute (3600 equals secs per minute)
-    Serial.print(':');
-    if ( (epoch % 60) < 10 ) {
-      // In the first 10 seconds of each minute, we'll want a leading '0'
-      Serial.print('0');
-    }
-    Serial.println(epoch % 60); // print the second
-  
-    return day;  
+  return day;  
   
 }
 
@@ -336,11 +273,11 @@ void ShowCon(int w) {
     matrix.writeDisplay();
 }
 
-void ShowDay() {
+void ShowDay(byte D) {
     matrix.writeDigitRaw(0,B00111111); // D
     matrix.writeDigitRaw(1,B01110111); // A
     matrix.writeDigitRaw(3,B01100110); // Y
-    matrix.writeDigitRaw(4,0);
+    matrix.writeDigitNum(4,int(D));
     matrix.drawColon(false);
     matrix.writeDisplay();
 }
